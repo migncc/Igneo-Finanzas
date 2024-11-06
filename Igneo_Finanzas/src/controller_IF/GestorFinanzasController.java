38cd51194225b5756dec075f6e06abdec5a8849f
Miguel <ignaciomiguel31@gmail.com>
2024-11-06 16:44:03 +0100

    Proyecto cambiado sin Maeven

--- Igneo_Finanzas/src/controller_IF/GestorFinanzasController.java
+++ Igneo_Finanzas/src/controller_IF/GestorFinanzasController.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controlador;
+package controller_IF;
 
-import modelo.Cuenta;
-import vista.MainFrame;
+import model_IF.Cuenta;
+import view_IF.MainFrame;
 
 /**
  *
--- Igneo_Finanzas/src/model_IF/Cuenta.java
+++ Igneo_Finanzas/src/model_IF/Cuenta.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package modelo;
+package model_IF;
 
 /**
  *
  * @author ignac
  */
-public class Transaccion {
+public class Cuenta {
     
 }
