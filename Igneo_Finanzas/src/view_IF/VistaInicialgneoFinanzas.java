261775b83f4ecb6a37b409263ca69f9694b11ec1
Miguel <ignaciomiguel31@gmail.com>
2024-11-13 19:14:34 +0100

    Creado BroderLyt

--- Igneo_Finanzas/src/view_IF/VistaInicialgneoFinanzas.java
+++ Igneo_Finanzas/src/view_IF/VistaInicialgneoFinanzas.java
@@ -26,22 +26,12 @@ public class VistaInicialgneoFinanzas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        etqIdea = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view_IF/Imagen menu principal.png"))); // NOI18N
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        etqIdea.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view_IF/Imagen menu principal.png"))); // NOI18N
+        getContentPane().add(etqIdea, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -83,6 +73,6 @@ public class VistaInicialgneoFinanzas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel etqIdea;
     // End of variables declaration//GEN-END:variables
 }
